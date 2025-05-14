b795d5b55ac7313a0708e44838b665d96e8085d3
Gustavo Oliveira <gugui2820@gmail.com>
2025-05-14 11:02:49 -0300

    modified:   ui/game_ui.py Erro no título da janela

--- ui/game_ui.py
+++ ui/game_ui.py
@@ -4,7 +4,7 @@ class GameUI:
     def __init__(self, game):
         self.game = game
         self.screen = game.surface
-        pygame.display.set_caption("Jogo da Memória - Rock Edition")
+        pygame.display.set_caption("Jogo da Memória")
         self.font = pygame.font.SysFont("Arial", 24)
         
     def draw(self):
